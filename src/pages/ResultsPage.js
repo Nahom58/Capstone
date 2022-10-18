c7d5bbd37b6a2cdcf343e19de11ceddb6c412b56
Nahom Agize <nahom@minerva.kgi.edu>
2022-10-18 16:00:24 -0300

    Results page Search bar implemented

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+import "../stylesheets/resultspage.css";
+import NavBar from '../components/NavBar';
 
 export default function ResultsPage() {
   return (
-    <div>ResultsPage</div>
+    <Container>
+    <Row>
+        <NavBar />
+    </Row>
+    <Row className="search-bar">
+      <Form className="d-flex">
+        <Form.Control
+          style={{ fontSize: 15, padding: 5 }}
+          type="search"
+          placeholder="Search By Name"
+          className="me-0"
+          id = "me-0"
+          aria-label="Search"
+          size = "lg"
+        />
+        <Form.Control
+          style={{ fontSize: 15, padding: 10 }}
+          type="search"
+          placeholder="Search by Speciality"
+          className="me-0"
+          aria-label="Search"
+          size = "lg"
+        />
+        <Form.Control
+          style={{ fontSize: 15, padding: 5 }}
+          type="filter"
+          placeholder="Filter"
+          className="me-1"
+          aria-label="Filter"
+          size = "lg"
+        />
+        <br> 
+        </br>
+        <Link to={{ pathname: "/ResultsPage" }}>
+          <Button variant="success" size = "lg">Search</Button>
+        </Link>
+      </Form>
+    </Row>
+    </Container>
   )
 }
