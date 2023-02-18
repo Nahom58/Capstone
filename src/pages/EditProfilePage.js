f2d0b183d888ffe0a48dde16520890c56b43c25f
Nahom Agize <nahom@minerva.kgi.edu>
2023-02-18 11:42:23 -0800

    Implement updating profile details

--- src/pages/EditProfilePage.js
+++ src/pages/EditProfilePage.js
@@ -4,7 +4,7 @@ import { Button, Col, Container, Form, Row } from 'react-bootstrap'
 import NavBar from '../components/NavBar'
 
 import { auth, db } from '../firebase';
-import { getDoc, doc } from "firebase/firestore";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
 
 import "../stylesheets/editprofile.css";
 import { onAuthStateChanged } from 'firebase/auth';
@@ -20,6 +20,16 @@ export default function EditProfilePage() {
         unspecified: 'Unspecified'
       };
 
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        address: '',
+        dob: '',
+        gender: '' 
+    })
+
     useEffect(() => {
         const listen = onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -30,6 +40,7 @@ export default function EditProfilePage() {
         
                 getDoc(docRef).then(async (doc) => {
                     setUserDetails({ ...doc.data(), id: doc.id })
+                    setFormData({ ...doc.data(), id: doc.id })
                 })
             } else {
                 setAuthUser(null);
@@ -39,6 +50,28 @@ export default function EditProfilePage() {
                 listen();
             }
     }, [authUserUid])
+
+    const handleChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+        console.log('formDatas', formData)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        const docRef = doc(db, 'users', `${authUserUid}`);
+        updateDoc(docRef, formData)
+          .then(() => {
+            console.log('Document successfully updated');
+            alert('Profile Updated Successfully')
+          })
+          .catch((error) => {
+            console.error('Error updating document: ', error);
+          });
+      }
     
   return (
     <div>
@@ -62,59 +95,58 @@ export default function EditProfilePage() {
             </Row>
             <Row>
             <Col sm={6} className="profileLeftSection">
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Row className="mb-3">
                     <Col>
                     <Form.Label>First Name</Form.Label>
-                    { userDetails? <Form.Control placeholder="-" defaultValue={userDetails.firstName} />:
+                    { userDetails? <Form.Control placeholder="-" name="firstName" defaultValue={userDetails.firstName} onChange={handleChange}/>:
                     <Form.Control placeholder="-" />}
                     </Col>
                     <Col>
                     <Form.Label>Last Name</Form.Label>
-                    { userDetails? <Form.Control placeholder="-" defaultValue={userDetails.lastName} />:
+                    { userDetails? <Form.Control placeholder="-" name="lastName" defaultValue={userDetails.lastName} onChange={handleChange} />:
                     <Form.Control placeholder="-" />}
                     </Col>
                 </Row>
         
                 <Form.Group className="mb-3" controlId="formGridDateOfBirth">
                     <Form.Label>Birth Date</Form.Label>
-                    { userDetails? <Form.Control type="date" placeholder="-" defaultValue={userDetails.dob} />:
+                    { userDetails? <Form.Control type="date" placeholder="-" name="dob" defaultValue={userDetails.dob} onChange={handleChange} />:
                     <Form.Control placeholder="-" />}
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formGridAddress">
+                <Form.Group className="mb-4" controlId="formGridAddress">
                 <Form.Label>Address</Form.Label>
-                { userDetails? <Form.Control placeholder="-" defaultValue={userDetails.address} />:
+                { userDetails? <Form.Control placeholder="-" name="address" defaultValue={userDetails.address} onChange={handleChange} />:
                     <Form.Control placeholder="-" />}
                 </Form.Group>
-        
-                <Button variant="success" type="submit" className="mb-3">
-                Save
-                </Button>
             </Form>
             </Col>
             <Col sm={6} className="profileRightSection">
 
-                
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formGridEmail">
                     <Form.Label>Email</Form.Label>
-                    { userDetails? <Form.Control type="email" placeholder="-" defaultValue={userDetails.email} />:
+                    { userDetails? <Form.Control type="email" placeholder="-" name="email" defaultValue={userDetails.email} onChange={handleChange} />:
                     <Form.Control placeholder="-" />}
                 </Form.Group>
                 
                 <Form.Group className="mb-3" controlId="formGridPhoneNumber">
                     <Form.Label>Phone Number</Form.Label>
-                    { userDetails? <Form.Control placeholder="-" defaultValue={userDetails.phone} />:
+                    { userDetails? <Form.Control placeholder="-" name="phone" defaultValue={userDetails.phone} onChange={handleChange} />:
                     <Form.Control placeholder="-" />}
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formGridGender">
+                <Form.Group className="mb-4" controlId="formGridGender">
                     <Form.Label>Gender</Form.Label>
-                    { userDetails? <Form.Select  value={userDetails.gender} name="gender" >
-                    <option value="">Select Gender</option>
-                    {Object.values(Gender).map((enumk) => (
-                        <option key={enumk} value={enumk}>{enumk}</option>
-                      ))}
+                    { userDetails? <Form.Select defaultValue={userDetails.gender} name="gender" onChange={handleChange} >
+                        {Object.values(Gender).map((enumk, index) => (
+                            index === 0 ? 
+                            <option key={enumk} value={enumk}>{enumk}</option> :
+                            userDetails.gender === enumk ? 
+                                <option key={enumk} value={enumk} selected>{enumk}</option> :
+                                <option key={enumk} value={enumk}>{enumk}</option>
+                        ))}
                     </Form.Select> : <Form.Select >
                     <option>Not selected</option>
                     <option>Male</option>
@@ -122,8 +154,15 @@ export default function EditProfilePage() {
                     <option>Unspecified</option>
                     </Form.Select>}
                 </Form.Group>
-                
+            </Form>
             </Col>
+            <Form onSubmit={handleSubmit}>
+            <div className='updateProfileButtonContainer'> 
+                <Button variant="success" type="submit" className="mb-3 updateProfileButton">
+                    Save
+                </Button>
+            </div>
+            </Form>
             </Row>
         </Container>
     </div>
